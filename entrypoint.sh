4349b3425528d07425dfda70747927e1eb3895f3
9M2PJU <9m2pju@gmail.com>
2024-09-23 13:57:29 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 . /app/venv/bin/activate
 
 # Add a cron job to fetch prayer times every day at 4 AM
-echo "0 4 * * * /app/venv/bin/python /app/fetch_prayer.py >> /var/log/cron_fetch_prayer.log 2>&1" >> /etc/crontabs/root
+# echo "0 4 * * * /app/venv/bin/python /app/fetch_prayer.py >> /var/log/cron_fetch_prayer.log 2>&1" >> /etc/crontabs/root
 
 # Run the fetch_prayer.py script to get initial prayer times
 python /app/fetch_prayer.py
